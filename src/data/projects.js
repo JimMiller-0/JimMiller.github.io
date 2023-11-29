fc8518b99cf45c157cc84124d3e8d5ee848f8e6a
Jim Miller <james.miller319@gmail.com>
2023-11-29 02:59:45 -0500

    link

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,8 +3,9 @@ const data = [
   {
     title: 'Merida',
     subtitle: 'LLM Application Security Service',
+    link: 'https://github.com/JimMiller-0/Merida',
     image: '/images/projects/merida.jpg',
-    date: '2015-11-20',
+    date: '2023-11-20',
     desc:
       'LLM Application Security Service (LASS) '
       + 'Mitigate Data Ecfiltratoin '
